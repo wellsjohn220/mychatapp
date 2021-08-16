75a0a7351f73dc2fc78153364102d979e190fe7b
wellsjohn220 <wellsjohn220@gmail.com>
2021-08-16 15:23:29 +1000

    new update version

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ app.use(express.static(__dirname))
 
 const port = process.env.PORT || 3010
 
-//app.listen(3010)
-
-//console.log('I am listening')
 var server = app.listen(port, () => {
     console.log('Server is listening on port', port)
 })
