1a58edfcf5b161f3d54148883bf4e829a94f574f
wellsjohn220 <wellsjohn220@gmail.com>
2021-08-17 15:00:42 +1000

    version week 6

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
-
+var bodyParser = require('body-parser')
 const app = express()
-app.use(express.static(__dirname))
 
 const port = process.env.PORT || 3010
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
 
 var messages = [
     {name: "John", message: "Hello from Sydney"}, 
@@ -14,6 +16,12 @@ var messages = [
 app.get('/messages', (req, res) => {
     //res.send("Hello World from server by John")
     res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    console.log(req.body)
+    //messages.push(req.body);
+    res.sendStatus(200);
 })
 
 var server = app.listen(port, () => {
