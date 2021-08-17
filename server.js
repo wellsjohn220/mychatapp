55810e940a1ef2fd1aba9c39440840f0c5852c6b
wellsjohn220 <wellsjohn220@gmail.com>
2021-08-17 12:28:46 +1000

    add more to index html

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ app.use(express.static(__dirname))
 
 const port = process.env.PORT || 3010
 
+var messages = [
+    {name: "John", message: "Hello from Sydney"}, 
+    {name: "Jane", message: "How are you"},
+    {name: "Rose", message: "Nice to see you"}
+]
+
+app.get('/messages', (req, res) => {
+    //res.send("Hello World from server by John")
+    res.send(messages)
+})
+
 var server = app.listen(port, () => {
     console.log('Server is listening on port', port)
 })
