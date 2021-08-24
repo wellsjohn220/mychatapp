6558cb598789b6c2189796ac2fc8121be4ab3210
wellsjohn220 <wellsjohn220@gmail.com>
2021-08-24 14:10:01 +1000

    upload for week serven

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.use(bodyParser.urlencoded({extended:false}))
 //mongodb+srv://Admin:<password>@chatapp.vrbrl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 var dbUrl = 'mongodb+srv://Admin:Sh268268@chatapp.vrbrl.mongodb.net/chatappdb?retryWrites=true&w=majority'
 
+var Message = mongoose.model('Message', {
+    name : String, message: String
+})
 var messages = [
     {name: "John", message: "Hello from Sydney send by hard code"}, 
     {name: "Rose", message: "Nice to see you send by hard code"}
@@ -24,16 +27,26 @@ app.get('/messages', (req, res) => {
     res.send(messages)
 })
 app.post('/messages', (req, res) => {
-    console.log(req.body)
-    messages.push(req.body);
-    io.emit('message', req.body);
-    res.sendStatus(200);
+    var message = new Message(req.body)
+    message.save((err) => {
+        if(err)
+        res.sendStatus(500);
+
+        console.log(req.body)
+        messages.push(req.body);
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    })   
 })
 
 mongoose.connect(dbUrl, (err) => {
     //if (err) return console.log(err);
     console.log('mongodb connection successful')
 })
+
+// socket.on("connect", () => {
+//   console.log(socket.connected); // true
+// });
 
 var server = app.listen(port, () => {
     console.log('Server is listening on port', port)
